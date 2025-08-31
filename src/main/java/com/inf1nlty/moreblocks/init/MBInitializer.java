fde75efc5854449c0b05d2a6339f621b636a6a73
Inf1nity <1449896523@qq.com>
2025-08-31 13:37:13 +0800

    Add colored cement bucket recipe

--- src/main/java/com/inf1nlty/moreblocks/init/MBInitializer.java
+++ src/main/java/com/inf1nlty/moreblocks/init/MBInitializer.java
@@ -5,21 +5,41 @@ import btw.crafting.recipe.RecipeManager;
 import btw.item.BTWItems;
 import com.inf1nlty.moreblocks.block.MBBlocks;
 import com.inf1nlty.moreblocks.item.MoreBlocksItems;
+import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 
 public class MBInitializer {
+
+    private static final int[] DYE_META_FOR_COLOR = {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
     public static void initMBRecipes() {
         addCraftingRecipes();
 //        addCampfireRecipes();
 //        addCrucibleRecipes();
-//        addCauldronRecipes();
+        addCauldronRecipes();
 //        addMillstoneRecipes();
 //        addOvenRecipes();
         addSoulforgeRecipes();
     }
 
     private static void addCraftingRecipes() {
+    }
+
+    private static void addCauldronRecipes() {
+
+        for (int color = 0; color < 16; color++) {
+            ItemStack[] outputs = new ItemStack[]{
+                    new ItemStack(MoreBlocksItems.colored_cement_bucket, 1, color),
+                    new ItemStack(Item.glassBottle, 1)
+            };
+            ItemStack[] inputs = new ItemStack[]{
+                    new ItemStack(BTWItems.cementBucket),
+                    new ItemStack(Item.bone),
+                    new ItemStack(Item.dyePowder, 1, DYE_META_FOR_COLOR[color]),
+                    new ItemStack(Item.potion, 1, 0)
+            };
+            RecipeManager.addCauldronRecipe(outputs, inputs);
+        }
     }
 
     private static void addSoulforgeRecipes() {
