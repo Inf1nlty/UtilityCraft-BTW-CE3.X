dd57dc59ee3c2faa3abcb156dd966d1ac8a6802f
Inf1nity <1449896523@qq.com>
2025-09-27 18:42:30 +0800

    Add sweepingEdgeScroll trade

--- src/main/java/com/inf1nlty/utilitycraft/UCEnchantments.java
+++ src/main/java/com/inf1nlty/utilitycraft/UCEnchantments.java
@@ -1,5 +1,6 @@
 package com.inf1nlty.utilitycraft;
 
+import btw.entity.mob.villager.trade.TradeProvider;
 import btw.item.BTWItems;
 import com.inf1nlty.utilitycraft.item.ISweepAttack;
 import net.minecraft.src.*;
@@ -8,7 +9,7 @@ public class UCEnchantments extends Enchantment {
 
     public static UCEnchantments sweepingEdge;
     public static final int SWEEPING_EDGE_ID = 101;
-    public static ItemStack arcaneSoulMendingScroll;
+    public static ItemStack sweepingEdgeScroll;
 
     public UCEnchantments(int id, int weight) {
         super(id, weight, EnumEnchantmentType.all);
@@ -18,7 +19,41 @@ public class UCEnchantments extends Enchantment {
 
     public static void registerEnchantments() {
         sweepingEdge = new UCEnchantments(SWEEPING_EDGE_ID, 2);
-        arcaneSoulMendingScroll = new ItemStack(BTWItems.arcaneScroll, 1, SWEEPING_EDGE_ID);
+        sweepingEdgeScroll = new ItemStack(BTWItems.arcaneScroll, 1, SWEEPING_EDGE_ID);
+
+        EntityVillager.removeCustomTrade(3,
+                TradeProvider.getBuilder()
+                        .name("btw:sell_unbreaking_scroll")
+                        .profession(3)
+                        .level(5)
+                        .arcaneScroll()
+                        .scrollEnchant(Enchantment.unbreaking)
+                        .secondaryEmeraldCost(48, 64)
+                        .mandatory()
+                        .build()
+        );
+
+        TradeProvider.getBuilder()
+                .name("btw:sell_unbreaking_scroll")
+                .profession(3)
+                .level(5)
+                .arcaneScroll()
+                .scrollEnchant(Enchantment.unbreaking)
+                .secondaryEmeraldCost(48, 64)
+                .mandatory()
+                .condition(villager -> villager.getRNG().nextFloat() < 0.5f)
+                .addToTradeList();
+
+        TradeProvider.getBuilder()
+                .name("ucBlacksmithSweepingEdgeScroll")
+                .profession(3)
+                .level(5)
+                .arcaneScroll()
+                .scrollEnchant(UCEnchantments.sweepingEdge)
+                .secondaryEmeraldCost(32, 64)
+                .mandatory()
+                .condition(villager -> villager.getRNG().nextFloat() >= 0.5f)
+                .addToTradeList();
     }
 
     @Override
